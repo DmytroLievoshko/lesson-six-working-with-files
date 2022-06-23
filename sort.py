f4845850513fdffd29995dca90711997f9020172
DmytroLievoshko <levka296@gmail.com>
2022-06-23 11:11:22 +0300

    added check of passed arameters

--- sort.py
+++ sort.py
@@ -3,7 +3,23 @@ import sys
 
 
 def main():
-    pass
+
+    quantity_args = len(sys.argv)-1
+    if quantity_args < 1 or quantity_args > 1:
+        print(
+            f'The script must take 1 argument! now there are {quantity_args} arguments.')
+        return
+
+    user_path = pathlib.Path(sys.argv[1])
+    if user_path.exists():
+
+        if user_path.is_dir():
+            print(str(user_path.absolute()))
+        else:
+            print(f'{str(user_path.absolute())} is no directory')
+
+    else:
+        print(f'{str(user_path.absolute())} does not exist')
 
 
 if __name__ == '__main__':
